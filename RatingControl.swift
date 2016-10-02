c06f46891927213452399c4246c5ddac91f9e4e2
Aastha Anand <aastha24@bu.edu>
2016-10-02 00:11:58 -0400

    Add files via upload

---
+++ RatingControl.swift
@@ -0,0 +1,15 @@
+//
+//  RatingControl.swift
+//  Foodagram
+//
+//  Created by Aastha Anand on 10/1/16.
+//  Copyright © 2016 Aastha Anand. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+    
+    
+    
+}
